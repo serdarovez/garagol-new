5ad14264934317a55663cdbbdd95b1a465ca0e23
seriovez <sahmedovicho@gmail.com>
2025-04-29 01:20:57 +0500

    asdas

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,26 +1,27 @@
 import useScrollToElement from "../hooks/useScrollToElement";
-import Cards from "../components/Cards";
-import ConnectUs from "../components/ConnectUs";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import Gurantee from "../components/Gurantee";
-import Home from "../components/Home";
-import InfiniteScrollingText from "../components/InfinitiveText";
-import Pricing from "../components/Pricing";
+// import Cards from "../components/Cards";
+// import ConnectUs from "../components/ConnectUs";
+// import Contact from "../components/Contact";
+// import Footer from "../components/Footer";
+// import Gurantee from "../components/Gurantee";
+// import Home from "../components/Home";
+// import InfiniteScrollingText from "../components/InfinitiveText";
+// import Pricing from "../components/Pricing";
 
 const Main = () => {
   useScrollToElement();
 
   return (
-    <div className="">
-      <Home />
+    <div className="mt-40">
+      hello world
+      {/* <Home />
       <Cards />
       <Pricing />
       <ConnectUs />
       <InfiniteScrollingText />
       <Gurantee />
       <Contact />
-      <Footer type="black" />
+      <Footer type="black" /> */}
     </div>
   );
 };
